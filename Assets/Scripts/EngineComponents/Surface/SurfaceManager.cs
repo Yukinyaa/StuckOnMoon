fc1a1d1be8f1f6554d46e354693f06879f91a959
yukinyaa <yukinyaa712@gmail.com>
2021-04-14 22:36:20 +0900

    SurfaceInputController>SurfaceManager

--- Assets/Scripts/EngineComponents/Surface/SurfaceManager.cs
+++ Assets/Scripts/EngineComponents/Surface/SurfaceManager.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
-
-public class SurfaceInputColtroller : MonoBehaviour
+public class SurfaceManager : MonoBehaviour
 {
     [SerializeField]
     public KeyCode placeKeyCode;
+    [SerializeField]
+    Vector3 gridOffset = Vector3.zero;
+    [SerializeField]
+    Vector3 gridSize = Vector3.one;
+
 
 #if UNITY_EDITOR
     public bool isDrawGridHintsEnabled;
@@ -41,9 +48,13 @@ public class SurfaceInputColtroller : MonoBehaviour
 #if UNITY_EDITOR
         if (isDrawGridHintsEnabled)
         {
-            for (int x = -10; x <= 10; x++)
-                for (int y = -10; y <= 10; y++)
- 
+            for (int x = -4; x <= 4; x++)
+                for (int y = -4; y <= 4; y++)
+                {
+                    DebugExtension.DrawPoint(new Vector3(gridOffset.x + gridSize.x * x, gridOffset.y + gridSize.y * y));
+                    Handles.Label(new Vector3(gridOffset.x + gridSize.x * (x + 0.5f), gridOffset.y + gridSize.y * (y + 0.5f)), $"{x},{y}", GUIStyle.none);
+                }
+                    
 
             DebugExtension.DrawArrow(Vector3.zero, new Vector3(gridSize.x, 0), Color.red);
             DebugExtension.DrawArrow(Vector3.zero, new Vector3(0, gridSize.y), Color.green);
