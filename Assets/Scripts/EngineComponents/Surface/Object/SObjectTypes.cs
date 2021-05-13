96f646110b70503dbdddc9b9cd3204cc3650cc46
yukinyaa <yukinyaa712@gmail.com>
2021-05-13 14:35:34 +0900

    Installed Input System

--- Assets/Scripts/EngineComponents/Surface/Object/SObjectTypes.cs
+++ Assets/Scripts/EngineComponents/Surface/Object/SObjectTypes.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Entities;
-using Unity.Collections;
 using Unity.Mathematics;
 
 public static class SObjectTypes
